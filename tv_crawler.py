d19824f8d88b8c60df4ad523c1c3bd092364ccc9
Yulun <yulunliss001@gmail.com>
2023-04-24 14:45:45 -0400

    modified file structure, and found more sites for scraping

--- tv_crawler.py
+++ tv_crawler.py
@@ -5,11 +5,21 @@ from urllib import parse
 import requests
 import json
 
+class Site:
+    def __init__(self:str, link:str, key_path:str, page:str, info:str):
+        self.link = link
+        self.key_path = key_path
+        self.page = int(page)
+        self.info = info
+
+    def __str__(self):
+        return 'Link: '+self.link+'\nKey path: '+self.key_path+'\nPage: '+self.page+'\nInfo: '+self.info+'\n'
+
 # retrieves all links in a given link
-def get_links(url:str)->list[str]:
+def get_links(url:str, key_path:str)->list[str]:
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
-    return [parse.urljoin(url, link.get('href')) for link in soup.find_all('a', href=re.compile('.*film/.*'))]
+    return [parse.urljoin(url, link.get('href')) for link in soup.find_all('a', href=re.compile('.*'+key_path+'.*'))]
 
 def get_content(url:str):
     data = {}
@@ -24,19 +34,17 @@ def get_content(url:str):
             key = key.strip()
             value = value.strip()
             data[key] = value
+        data['Link'] = url
     return data
     # return [parse.urljoin(url, link.get('href')) )]
 
-def crawl_n_scrape(url:str, out_file:TextIOWrapper, max_content=100):
-    visited = []
-    links = get_links(url)
-    for link in links:
-        content = get_content(link)
-        print(content, file=out_file)
-        print("processing: ",link)
-        # max_content = max_content - 1
-        # print(link)
-        # print(link)
+def crawl_n_scrape(site:Site, out_file:TextIOWrapper):
+    for i in range(1):
+        links = get_links(site.link+str(i+1), site.key_path)
+        for link in links:
+            # content = get_content(link)
+            print(link, file=out_file)
+            print("processing: ",link)
         
     
 
@@ -45,19 +53,33 @@ def writelines(filename, data):
         for d in data:
             print(d, file=fout)
 
-# read file into list[str], each elem is a line
-def get_roots(file_name: str) -> list[str]:
-    with open(file_name, 'r') as file:
-        lines = file.readlines()
-    lines = [line.strip() for line in lines if line.strip()]
-    # print(lines)
-    return lines
+# read file into list[Site]
+def get_sites(filename)->list[Site]:
+    with open(filename, 'r') as f:
+        lines = f.read().splitlines()
+
+    sites = []
+    site = None
+    for line in lines:
+        if line.startswith('Link: '):
+            link = line.split('Link: ')[1]
+            site = Site(link, '', '0', '')
+        elif line.startswith('Key path: '):
+            site.key_path = line.split('Key path: ')[1]
+        elif line.startswith('Page: '):
+            site.page = line.split('Page: ')[1]
+        elif line.startswith('Info: '):
+            site.info = line.split('Info: ')[1]
+            sites.append(site)
+            print(site)
+
+    return sites
+
 
 if __name__ == '__main__':
-    roots = get_roots("roots.txt")
+    sites = get_sites("roots.txt")
 
     with open('links.txt', 'w') as fout:
-        for link in roots:
-            for i in range(1):
-                crawl_n_scrape(link+str(i+1), fout)
+        for site in sites:
+            crawl_n_scrape(site, fout)
 
