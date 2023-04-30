3aee0b173f42f67de85358368f517c960b0d4979
Yulun <yulunliss001@gmail.com>
2023-04-30 14:49:09 -0400

    test

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let search = document.querySelector("#search")
+let search = document.getElementById("search")
+let test = document.getElementById("test")
+
 search.addEventListener("click", function () {
+    alert("hello")
+})
+test.addEventListener("click", function () {
     alert("hello")
 })
